90d99addb4fc791b2460e1d44c219e5ae9983afb
Evan Hallmark <ehallmar@usc.edu>
2022-01-30 13:41:01 -0800

    debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
-
+PROJECT_NAME=ehallmarksolutions
 IMAGE_NAME="sports-scraper:latest"
+FULL_IMAGE_NAME="gcr.io/$PROJECT_NAME/$IMAGE_NAME"
 
 echo "Building image"
-docker build -t $IMAGE_NAME -f Dockerfile .
+docker build -t $FULL_IMAGE_NAME -f Dockerfile .
+
+docker push $FULL_IMAGE_NAME
